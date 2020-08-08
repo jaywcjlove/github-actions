d3425c7f6d3056aad29fde475e1b24ec202e370e
jaywcjlove <398188662@qq.com>
2020-08-08 10:12:16 +0800

    feat: 测试nodejs中获取环境变量

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log('process.env:NODE_API_AUTH_TOKEN:', process.env.NODE_API_AUTH_TOKEN)
+console.log('process.env:NODE_API_TOKEN:', process.env.NODE_API_TOKEN)
 console.log('process.env:NODE_AUTH_TOKEN:', process.env.NODE_AUTH_TOKEN)
 console.log('process.env:', process.env)
